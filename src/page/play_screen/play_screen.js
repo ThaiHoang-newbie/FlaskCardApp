9dd63004087fafc73a7348af82e553e337a07366
Thai Hoang <thaihoang20112k3@gmail.com>
2023-09-28 20:30:22 +0700

    Move data to another page

--- src/page/play_screen/play_screen.js
+++ src/page/play_screen/play_screen.js
@@ -5,37 +5,15 @@ import Header from '../../components/header/header';
 import Small_button from '../../components/buttons/small_buttons';
 import Big_button from '../../components/buttons/big_buttons';
 import Word from '../../components/word/word';
+import init_flashcard from '../../data';
+// import darkMode from '../setting/setting';
 
 export default function Play_screen() {
   const [index, setIndex] = useState(0);
 
-
-  const init_flashcard = [
-    { word: "Dog", translation: "Chó" },
-    { word: "Cat", translation: "Mèo" },
-    { word: "Apple", translation: "Táo" },
-    { word: "Banana", translation: "Chuối" },
-    { word: "Car", translation: "Xe hơi" },
-    { word: "Book", translation: "Sách" },
-    { word: "House", translation: "Nhà" },
-    { word: "Hello", translation: "Xin chào" },
-    { word: "Goodbye", translation: "Tạm biệt" },
-    { word: "Friend", translation: "Bạn bè" },
-    { word: "Love", translation: "Yêu" },
-    { word: "Water", translation: "Nước" },
-    { word: "Food", translation: "Thức ăn" },
-    { word: "Phone", translation: "Điện thoại" },
-    { word: "Computer", translation: "Máy tính" },
-    { word: "Music", translation: "Âm nhạc" },
-    { word: "Movie", translation: "Phim" },
-    { word: "Time", translation: "Thời gian" },
-    { word: "Money", translation: "Tiền" },
-    { word: "Table", translation: "Cái bàn" },
-  ];
-
   const [flashcards, setFlashcards] = useState(init_flashcard);
   const cards = flashcards.length;
-  
+
   const Next = () => {
     setIndex((prevIndex) => (prevIndex + 1) % flashcards.length);
   }
@@ -59,6 +37,7 @@ export default function Play_screen() {
     setIndex(0);
   };
 
+  const darkMode = false;
 
   return (
     <View style={styles.container}>

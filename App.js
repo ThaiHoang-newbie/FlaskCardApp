32c1ecba95e77cd2c0927927455faac33be4e77d
Thai Hoang <thaihoang20112k3@gmail.com>
2023-09-28 20:28:48 +0700

    App

--- App.js
+++ App.js
@@ -1,13 +1,48 @@
 import React from 'react';
-import { View } from 'react-native';
-
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Play_screen from './src/page/play_screen/play_screen';
+import Setting from './src/page/setting/setting';
+import { DarkModeProvider } from './src/DarkModeContext';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
 
   return (
-    <View>
-      <Play_screen></Play_screen>
-    </View>
+    <DarkModeProvider>
+      <NavigationContainer>
+        <Tab.Navigator
+          screenOptions={
+            ({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
+                if (route.name === 'Home') {
+                  iconName = focused ? 'home' : 'home-outline';
+                } else if (route.name === 'Setting') {
+                  iconName = focused ? 'ios-list' : 'ios-list-outline';
+                }
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: '#FF6969',
+              tabBarInactiveTintColor: 'gray',
+            })}
+        >
+          <Tab.Screen name="Home" component={Play_screen} />
+          <Tab.Screen name="Setting" component={Setting} />
+        </Tab.Navigator>
+
+      </NavigationContainer>
+    </DarkModeProvider>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+  },
+
+});

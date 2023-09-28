9ee91415c038b1bf76b03e40481e414e19cc62c0
Thai Hoang <thaihoang20112k3@gmail.com>
2023-09-28 20:29:50 +0700

    Dark mode context

---
+++ src/DarkModeContext.js
@@ -0,0 +1,23 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const DarkModeContext = createContext();
+
+export const useDarkMode = () => {
+  return useContext(DarkModeContext);
+};
+
+export const DarkModeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+export default DarkModeContext;
